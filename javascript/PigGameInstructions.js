b507638fe62803fc367824c6e81fd81bb2f8791c
leahrowland86 <50224313+leahrowland86@users.noreply.github.com>
2019-10-25 18:51:44 -0700

    Add files via upload

---
+++ javascript/PigGameInstructions.js
@@ -0,0 +1,8 @@
+function toggleText() {
+  var text = document.getElementById("instructions");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
